b1918788b6784499452b623399256dc8ae0a2c77
MchaseCov <67227108+MchaseCov@users.noreply.github.com>
2022-03-25 15:19:58 -0400

    update media folder

--- Media/Media.lua
+++ Media/Media.lua
@@ -2,9 +2,7 @@ local K, C = unpack(KkthnxUI)
 
 C["Media"] = {
 	["Sounds"] = {
-		ProcSound = [[Interface\AddOns\KkthnxUI\Media\Sounds\Proc.ogg]],
-		WarningSound = [[Interface\AddOns\KkthnxUI\Media\Sounds\Warning.ogg]],
-		WhisperSound = [[Interface\AddOns\KkthnxUI\Media\Sounds\Whisper.ogg]],
+		KillingBlow = [[Interface\AddOns\KkthnxUI\Media\Sounds\KillingBlow.ogg]],
 	},
 
 	["Backdrops"] = {
@@ -26,26 +24,18 @@ C["Media"] = {
 		CopyChatTexture = [[Interface\AddOns\KkthnxUI\Media\Chat\Copy.tga]],
 		GlowTexture = [[Interface\AddOns\KkthnxUI\Media\Textures\GlowTex.tga]],
 		LogoTexture = [[Interface\AddOns\KkthnxUI\Media\Textures\Logo.tga]],
+		LogoSmallTexture = [[Interface\AddOns\KkthnxUI\Media\Textures\LogoSmall.tga]],
 		MouseoverTexture = [[Interface\AddOns\KkthnxUI\Media\Textures\Mouseover.tga]],
 		NewClassIconsTexture = [[Interface\AddOns\KkthnxUI\Media\Unitframes\NEW-ICONS-CLASSES.blp]],
 		Spark128Texture = [[Interface\AddOns\KkthnxUI\Media\Textures\Spark_128]],
 		Spark16Texture = [[Interface\AddOns\KkthnxUI\Media\Textures\Spark_16]],
-
-		BlueArrow = [[Interface\AddOns\KkthnxUI\Media\Nameplates\BlueArrow.blp]],
-		BlueArrow2 = [[Interface\AddOns\KkthnxUI\Media\Nameplates\BlueArrow2.blp]],
-		NeonGreenArrow = [[Interface\AddOns\KkthnxUI\Media\Nameplates\NeonGreenArrow.blp]],
-		NeonRedArrow = [[Interface\AddOns\KkthnxUI\Media\Nameplates\NeonRedArrow.blp]],
-		PurpleArrow = [[Interface\AddOns\KkthnxUI\Media\Nameplates\PurpleArrow.blp]],
-		RedArrow = [[Interface\AddOns\KkthnxUI\Media\Nameplates\RedArrow.blp]],
-		RedArrow2 = [[Interface\AddOns\KkthnxUI\Media\Nameplates\RedArrow2.blp]],
-		RedChevronArrow = [[Interface\AddOns\KkthnxUI\Media\Nameplates\RedChevronArrow.blp]],
-		RedChevronArrow2 = [[Interface\AddOns\KkthnxUI\Media\Nameplates\RedChevronArrow2.blp]],
+		TargetIndicatorTexture = [[Interface\AddOns\KkthnxUI\Media\Nameplates\TargetIndicatorArrow.blp]],
 	},
 
 	["Fonts"] = {
 		BlankFont = [[Interface\AddOns\KkthnxUI\Media\Fonts\Invisible.ttf]],
-		DamageFont = [[Interface\AddOns\KkthnxUI\Media\Fonts\Damage.ttf]],
-		KkthnxUIFont = [[Interface\AddOns\KkthnxUI\Media\Fonts\Normal.ttf]],
+		DamageFont = DAMAGE_TEXT_FONT,
+		KkthnxUIFont = STANDARD_TEXT_FONT,
 	},
 
 	["Statusbars"] = {
@@ -58,18 +48,10 @@ C["Media"] = {
 		KkthnxUIStatusbar = [[Interface\AddOns\KkthnxUI\Media\Textures\Statusbar]],
 		PaloozaStatusbar = [[Interface\AddOns\KkthnxUI\Media\Textures\Palooza.tga]],
 		SkullFlowerUIStatusbar = [[Interface\AddOns\KkthnxUI\Media\Textures\SkullFlowerUI.tga]],
-		ToxiUIStatusbar = [[Interface\AddOns\KkthnxUI\Media\Textures\ToxiUI.tga]],
 		TukuiStatusbar = [[Interface\AddOns\KkthnxUI\Media\Textures\ElvTukUI.tga]],
 		ZorkUIStatusbar = [[Interface\AddOns\KkthnxUI\Media\Textures\ZorkUI.tga]],
 	},
 }
-
-if (K.Client == "koKR" or K.Client == "zhTW" or K.Client == "zhCN") then
-	C["Media"].Fonts.KkthnxUIFont = STANDARD_TEXT_FONT
-	C["Media"].Fonts.DamageFont = DAMAGE_TEXT_FONT
-elseif (K.Client ~= "enUS" and K.Client ~= "frFR" and K.Client ~= "enGB") then
-	C["Media"].Fonts.DamageFont = DAMAGE_TEXT_FONT
-end
 
 -- Register Borders
 for name, path in pairs(C["Media"].Borders) do
